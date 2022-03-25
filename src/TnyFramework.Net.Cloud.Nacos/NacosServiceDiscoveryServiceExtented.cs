9629111a0a62638f9ed068c0526c024f2f740cee
YangKun <yangkun311@gmail.com>
2022-03-25 16:25:39 +0800

    build:
    - 简化 build 脚本配置

--- src/TnyFramework.Net.Cloud.Nacos/NacosServiceDiscoveryServiceExtented.cs
+++ src/TnyFramework.Net.Cloud.Nacos/NacosServiceDiscoveryServiceExtented.cs
@@ -1,12 +1,18 @@
+#region
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Nacos.AspNetCore.V2;
 using Nacos.V2.DependencyInjection;
 using TnyFramework.DI.Container;
 
+#endregion
+
 namespace TnyFramework.Net.Cloud.Nacos
 {
-    public static class GenericHostNacosServerDiscoveryServiceExtensions
+
+    public static class GenericHostNacosServerServiceExtensions
     {
         public static IHostBuilder UseNacosServerDiscovery(this IHostBuilder builder, string section = "nacos")
         {
@@ -17,5 +23,23 @@ namespace TnyFramework.Net.Cloud.Nacos
                 services.AddSingletonUnit<NacosServerDiscoveryService>();
             });
         }
+
+
+        public static IHostBuilder UseNacosConfiguration(this IHostBuilder builder, string section = "nacos")
+        {
+            return builder.ConfigureAppConfiguration((context, configBuilder) => {
+                var root = configBuilder.Build();
+                configBuilder.AddNacosV2Configuration(root.GetSection(section));
+            });
+        }
+
+
+        public static IHostBuilder UseNacos(this IHostBuilder builder, string section = "nacos")
+        {
+            return builder
+                .UseNacosServerDiscovery(section)
+                .UseNacosConfiguration(section);
+        }
     }
+
 }
